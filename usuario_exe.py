247762cd15f2c91929c49e41a0d42358a0e51550
naddyAG <naddyaranibar@gmail.com>
2024-12-14 17:41:06 -0400

    14-12-2024
    Adición de código faltante al archivo "ususario_exe.py" y verificación

---
+++ usuario_exe.py
@@ -0,0 +1,9 @@
+from usuario import listar_usuarios,insertar_usuario
+usuarios=listar_usuarios()
+print (usuarios)
+
+nombre = input("Nombre: ")
+apellido = input("Apellido: ")
+nombre_usuario = input("Nombre_Usuario: ")
+email = input("E-mail: ")
+insertar_usuario(nombre, apellido,nombre_usuario,email)
